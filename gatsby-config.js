84f7f1b1378525d1605f72a9c27adb5652c1bf1a
Dara Heng <daragitgud@gmail.com>
2021-02-10 23:54:40 +1000

    chore: update tab icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,7 +121,7 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         // edit below
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `src/assets/me.svg`,
       },
     },
     {
