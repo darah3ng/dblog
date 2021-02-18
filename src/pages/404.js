48322f4f7743414232ba575f75c10a03a4ec776b
Dara Heng <daragitgud@gmail.com>
2021-02-18 23:25:42 +1000

    feat: update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,12 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Button from "../components/button"
+
+import PageNotFoundImage from '../assets/notfound.svg';
 
 class NotFoundPage extends React.Component {
   render() {
@@ -13,7 +17,14 @@ class NotFoundPage extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
+        
+        <PageNotFoundImage style={{ maxHeight: 300, maxWidth: 300 }} />
+
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+        <Link to="/">
+          <Button marginTop="35px">Home 👈 😬</Button>
+        </Link>
       </Layout>
     )
   }
